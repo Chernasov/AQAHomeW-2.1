f22bdc77c4c687ccd195004a155ac357ec157631
Timofey Chernasov <timofeychernasov@gmail.com>
2021-10-31 20:13:33 +0300

    Added tests for negative

--- src/test/java/ru/netology/card/ChromeTest.java
+++ src/test/java/ru/netology/card/ChromeTest.java
@@ -49,5 +49,39 @@ class ChromeTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void shouldTestNegativeFIO() {
+        driver.get("http://localhost:9999");
+        driver.findElement(cssSelector("[data-test-id='name'] input")).sendKeys("Micael");
+        driver.findElement(cssSelector("[data-test-id='phone'] input")).sendKeys("+79101105214");
+        driver.findElement(cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(cssSelector("[type='button']")).click();
+        String actual = driver.findElement(cssSelector(".input_invalid .input__sub")).getText().trim();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldTestNegativePhone() {
+        driver.get("http://localhost:9999");
+        driver.findElement(cssSelector("[data-test-id='name'] input")).sendKeys("Дмитрий");
+        driver.findElement(cssSelector("[data-test-id='phone'] input")).sendKeys("+7910110521499");
+        driver.findElement(cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(cssSelector("[type='button']")).click();
+        String actual = driver.findElement(cssSelector(".input_invalid .input__sub")).getText().trim();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldTestNegativeCheckbox() {
+        driver.get("http://localhost:9999");
+        driver.findElement(cssSelector("[data-test-id='name'] input")).sendKeys("Дмитрий");
+        driver.findElement(cssSelector("[data-test-id='phone'] input")).sendKeys("+70123456789");
+        driver.findElement(cssSelector("[type='button']")).click();
+        String actual = driver.findElement(cssSelector(".input_invalid .checkbox__text")).getCssValue("color");
+        String expected = "rgba(255, 92, 92, 1)";
+        assertEquals(expected, actual);
+    }
 }
 
